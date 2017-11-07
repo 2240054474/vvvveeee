fb7fcd40498a496a6ea01c02567f54c9f094cc07
刘宇航 <Netban@NetBanMacBook-Pro.local>
2017-11-07 15:20:25 +0800

    first commit

---
+++ Simona_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simona_Test
+//
+//  Created by Netban on 17/11/7.
+//  Copyright © 2017年 Simona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simona_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simona_Test
+//
+//  Created by Netban on 17/11/7.
+//  Copyright © 2017年 Simona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
